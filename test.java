fcc44163b385fc8c6455ef396b284dbf525f705e
bexter1999 <bexter125@naver.com>
2022-06-27 19:14:46 +0900

    practice1

---
+++ test.java
@@ -0,0 +1,20 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+	int score = 95;
+	
+	if(score >= 90) {
+		System.out.println("점수가 90점 이상입니다. ");
+		System.out.println("점수가 A등급 입니다.");
+	}
+	if(score < 90) {
+		System.out.println("점수가 90점 아래입니다. ");
+		System.out.println("점수가 B등급 입니다. ");
+	}
+		
+
+	}
+
+}
